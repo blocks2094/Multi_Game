d2fa0678167208d50f5dd9c01ec4532504728743
HyunWoo <blocks2094@g.shingu.ac.kr>
2026-07-05 13:04:30 +0900

    [Feat] 3- 예측

--- Multi_Game/Assets/Scripts/BasicSpawner.cs
+++ Multi_Game/Assets/Scripts/BasicSpawner.cs
@@ -31,6 +31,12 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
         }
     }
 
+    private bool _mouseButton0;
+    private void Update()
+    {
+        _mouseButton0 = _mouseButton0 | Input.GetMouseButton(0);
+    }
+
     public void OnInput(NetworkRunner runner, NetworkInput input)
     {
         var data = new NetworkInputData();
@@ -46,6 +52,9 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
 
         if (Input.GetKey(KeyCode.D))
             data.direction += Vector3.right;
+
+        data.buttons.Set(NetworkInputData.MOUSEBUTTON0, _mouseButton0);
+        _mouseButton0 = false;
 
         input.Set(data);
     }
